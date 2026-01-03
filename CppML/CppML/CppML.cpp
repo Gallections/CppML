855ade08c9e9849902455f697d9f85e6d08dca32
Gallections <Gallections@gmail.com>
2026-01-02 22:03:21 -0800

    feat: initial commit

---
+++ CppML/CppML/CppML.cpp
@@ -0,0 +1,9 @@
+// CppML.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "This is the start of the CppML library" << std::endl;
+}
